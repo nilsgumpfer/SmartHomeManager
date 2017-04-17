a44acb140a97dfa09da3c6f2b9dc050749d6a32e
Tim <tim-rosenbecker@gmx.de>
2017-04-17 17:31:46 +0200

    SmartHomeManager GUI

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("GUI.fxml"));
+        primaryStage.setTitle("SmartHomeManager for SmartHome CGR");
+        primaryStage.setScene(new Scene(root, 640, 480));
+        primaryStage.show();
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+
+    }
+}
