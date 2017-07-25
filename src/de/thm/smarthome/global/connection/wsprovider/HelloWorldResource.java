35df43ad841d1ba698c6683052af82aa4364d8fa
Nils Gumpfer <nilsgumpfer94@arcor.de>
2017-07-25 18:34:51 +0200

    Karleparametr hinzugefügt xD

--- src/de/thm/smarthome/global/connection/wsprovider/HelloWorldResource.java
+++ src/de/thm/smarthome/global/connection/wsprovider/HelloWorldResource.java
@@ -2,6 +2,7 @@ package de.thm.smarthome.global.connection.wsprovider;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -11,10 +12,10 @@ import javax.ws.rs.core.MediaType;
  */
 @Path("ws")
 public class HelloWorldResource {
-    
-    @GET @Path( "info" ) @Produces( MediaType.TEXT_PLAIN )
-    public String getHello() {
-        return "Hello World!";
+
+    @GET @Path( "info/karleparameter/{karleparameter}" ) @Produces( MediaType.TEXT_PLAIN )
+    public String getHello(@PathParam( "karleparameter" ) String karleparameter) {
+        return "Hello World, " + karleparameter;
     }
 
 }
